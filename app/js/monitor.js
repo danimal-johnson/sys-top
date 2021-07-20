5a9b9813ba2c956b8182ed887e22fd49eb04cf23
Dan Johnson <danimal.johnson@gmail.com>
2021-07-20 05:46:00 -0700

    Load all data. Update CPU usage and uptime every 2 seconds.

---
+++ app/js/monitor.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const osu = require('node-os-utils');
+const cpu = osu.cpu;
+const mem = osu.mem;
+const os = osu.os;
+
+// Run every 2 seconds
+setInterval(() => {
+  // Get CPU usage
+  cpu.usage().then((info) => {
+    document.getElementById('cpu-usage').innerText = `${info}%`;
+  });
+
+  // Get CPU free
+  cpu.free().then((info) => {
+    document.getElementById('cpu-free').innerText = `${info}%`;
+  });
+
+  // Get system Uptime
+  document.getElementById('sys-uptime').innerText = formatTime(os.uptime());
+
+}, 2000);
+
+// Set static system info
+document.getElementById('cpu-model').innerText = cpu.model();
+document.getElementById('comp-name').innerText = os.hostname();
+document.getElementById('os').innerText = `${os.type()} ${os.arch()}`;
+document.getElementById('sys-uptime').innerText = os.uptime();
+mem.info().then(info => {
+  document.getElementById('mem-total').innerText =
+    `${info.totalMemMb} MB`;
+});
+
+// Show days, hours, minutes and seconds
+function formatTime(seconds) {
+  const days = Math.floor(seconds / 86400);
+  const hours = Math.floor((seconds % 86400) / 3600);
+  const minutes = Math.floor(((seconds % 86400) % 3600) / 60);
+  const sec = ((seconds % 86400) % 3600) % 60;
+
+  return `${days}d ${hours}h ${minutes}m ${sec}s`;
+}
+
